71531e39492273b6a23512e4f4704f032f4c3eff
TMillsap04 <tmmillsap@student.fullsail.edu>
2023-05-20 21:02:52 -0500

    Working on Homepage.

--- astrum-site/src/pages/index.js
+++ astrum-site/src/pages/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+
+
+      <div>
+        <h2>Best Selling Crystals</h2>
+        <p>Beauties of many variations</p>
+      </div>
+
+      <div>
+        ['Product 1', 'Product 2', 'Product 3'].map(())
+      </div>
+
+    </>
   )
 }
 
-export default index;
+export default Home;
